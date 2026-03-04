76c8d48b18abcc8e4273a2b4273c5364f90be924
basild <basild@gmail.com>
2026-03-04 21:12:47 +0000

    Fixed base location for vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     // The base path should match your GitHub repository name exactly to ensure
     // static assets like /assets/player.png resolve natively on the git sub-path url.
-    base: 'FighterRoyale/',
+    base: '/fighter-royale/',
 });
